ca0302f5d253f526506ac9cc531f22180fe39d57
Thomas John Gulisao <fligno.thomas@gmail.com>
2018-06-18 14:57:25 +0800

    First Blood

---
+++ readme.md
@@ -0,0 +1,18 @@
+# TDD Training
+
+## Installation
+```
+git clone https://github.com/VerzatileTom/TDD-Training.git tdd-training
+cd tdd-training
+composer install
+cp .env.example .env
+php artisan key:generate
+```
+
+## Blog App Features
+- Users can post a blog
+- Users can update their own blog
+- Users can delete their own blog
+- Users can comment to a blog
+- Users can update their comment
+- Users and blog owner can delete a comment
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1 class="mb-4">Articles</h1>
+
+            <div class="card mb-3">
+                <div class="card-header">Hello World</div>
+
+                <div class="card-body">
+                    Lorem ipsum keme keme keme 48 years matod keme lulu paminta emena gushung pranella shontis mabaho 48 years at bakit otoko effem chapter ng at nang ang effem at bakit at ang ma-kyonget lulu at ang at bakit oblation matod at ang ma-kyonget matod sa bongga wiz at bakit bigalou Mike pinkalou at bakit kasi makyonget ng at bakit thunder bakit at at bakit jowabelles ano ng ma-kyonget shala kemerloo.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
