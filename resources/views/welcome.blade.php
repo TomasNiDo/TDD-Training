8944c5b9a31f9c1f4a938086473ecd7f64d09705
Thomas John Gulisao <fligno.thomas@gmail.com>
2018-06-23 12:44:57 +0800

    Add create button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <h1 class="mb-4">Articles</h1>
-
+            <a href="{{ url('/articles/create') }}" class="btn btn-primary mb-4">Add New Article</a>
             @foreach ($articles as $article)
             <div class="card mb-3">
                 <div class="card-header">
